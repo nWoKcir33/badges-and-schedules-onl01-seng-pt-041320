ae3d6b37c38bc81cd089be124dc535a434fb4c00
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 03:14:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,5 @@ def assign_rooms(attendees)
   attendees.each_with_index{|name,index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
   return room_assignments
 end
+
+def printer
