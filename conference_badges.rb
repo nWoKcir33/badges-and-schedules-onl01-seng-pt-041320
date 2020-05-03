8db4b4937b65070fd19beb68e584a4ec5215c96f
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 02:55:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.each{|name| puts "Hello, my name is #{name}."}
 end
