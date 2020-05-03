b9108e3ff9be4c5e9b89a9cb0f04f5f72f17eb01
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-03 03:12:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms(attendees)
   room_assignments = []
-  attendees.each_with_index{|name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  attendees.each_with_index{|name, index| room_assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
   return room_assignments
 end
